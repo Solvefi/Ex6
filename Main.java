270db6b22399b265f5ecd1ddb2008d25c91205b7
Solvefi <masha56777777@gmail.com>
2023-11-29 14:02:06 +0300

    update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+
+        int a =40;
+        int b = 60;
+        System.out.println(a+b);
     }
 }
 
