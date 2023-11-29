8d633604451925dde3343697b6aa91f26de1a7ed
Solvefi <masha56777777@gmail.com>
2023-11-29 14:11:14 +0300

    update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
+
+        int a=66;
+        int b=11;
+        System.out.println(a/b);
     }
 }
